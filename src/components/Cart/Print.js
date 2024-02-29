f978f27da19feb8239e36a065b34febc9e45ab72
cornel <cornel.siclovan@gmail.com>
2024-02-29 14:47:29 +0200

    total round

--- src/components/Cart/Print.js
+++ src/components/Cart/Print.js
@@ -108,7 +108,7 @@ const Print = (props) => {
           element.price + " x " +
           element.cartItem.quantity +
           "  " +
-          element.price * element.cartItem.quantity +
+          Math.round( element.price * element.cartItem.quantity + Number.EPSILON) +
           " RON";
         drawDeviceFont(textString, 0, y, "0", 1, 2, 0, 0, 0, 0);
 
